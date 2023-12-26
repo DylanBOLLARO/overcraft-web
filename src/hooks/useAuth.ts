72c116b544e0bed09871bb12984d4a5468915325
Dylan <bollarodylan@gmail.com>
2023-12-26 14:04:17 +0100

    .

--- src/hooks/useAuth.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo } from 'react'
-import { useSelector } from 'react-redux'
-import { selectCurrentUser } from '../utils/features/auth/authSlice'
-
-export const useAuth = () => {
-    const user = useSelector(selectCurrentUser)
-
-    return useMemo(() => ({ user }), [user])
-}
